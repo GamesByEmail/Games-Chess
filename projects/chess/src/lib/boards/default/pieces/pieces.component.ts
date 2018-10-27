ce9578350c53f7ad4a6160629c6398e083e35a6f
Scott Nesin <snesin@gmail.com>
2018-10-26 23:58:20 -0500

    progress...

--- projects/chess/src/lib/boards/default/pieces/pieces.component.ts
+++ projects/chess/src/lib/boards/default/pieces/pieces.component.ts
@@ -4,7 +4,7 @@ import { ITemplateLibrary } from '@gamesbyemail/base';
 import { ITemplateSize } from '@gamesbyemail/base';
 import { TemplateLibraryService } from '@gamesbyemail/base';
 
-export interface IKey {
+export interface IPieceKey {
   type: 'Pawn' | 'Knight' | 'Bishop' | 'Rook' | 'Queen' | 'King';
   team: 'White' | 'Black';
   small?: boolean;
@@ -21,7 +21,7 @@ export class PiecesComponent implements ITemplateLibrary {
   constructor(private templateLibraryService: TemplateLibraryService) {
   }
 
-  getTemplate(key: IKey): TemplateRef<any> | undefined {
+  getTemplate(key: IPieceKey): TemplateRef<any> | undefined {
     return this.templateLibraryService.findTemplateById(this.templateRefs, key.type + key.team + (key.type === 'King' && key.resigned ? 'Resigned' : '') + (key.small ? 'Small' : ''));
   }
   getSize(template: TemplateRef<any> | undefined): ITemplateSize | undefined {
--- projects/chess/src/lib/chess.module.ts
+++ projects/chess/src/lib/chess.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatRadioModule, MatIconModule } from '@angular/material';
-import { StartGameModule,IconModule,SharedModule } from '@gamesbyemail/base';
+import { StartGameModule,IconModule,OptionTitleModule } from '@gamesbyemail/base';
 import { StartGameComponent } from './components/start-game/start-game.component';
 import { StartGameOptionsComponent } from './components/start-game-options/start-game-options.component';
 import { PiecesComponent } from './boards/default/pieces/pieces.component';
@@ -16,7 +16,7 @@ import { BoardComponent } from './boards/default/board/board.component';
     MatIconModule,
     StartGameModule,
     IconModule,
-    SharedModule
+    OptionTitleModule
   ],
   declarations: [
     StartGameComponent,
